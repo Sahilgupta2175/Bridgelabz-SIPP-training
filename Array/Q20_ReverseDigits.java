8d45ee83af49e7b7a494ca3487f36afc8e6325f7
Sahil Gupta <guptasahil2175@gmail.com>
2025-06-16 11:27:39 +0530

    [Sahil Gupta] Add Reverse digits

---
+++ Array/Q20_ReverseDigits.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Q20_ReverseDigits {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+
+        int[] digits = new int[10];
+        int index = 0;
+
+        while (number > 0 && index < digits.length) {
+            digits[index++] = number % 10;
+            number /= 10;
+        }
+
+        System.out.print("Reversed number: ");
+        for (int i = 0; i < index; i++) {
+            System.out.print(digits[i]);
+        }
+        sc.close();
+    }
+}
