0f8dfd2884f91938254ce8b93f64a899dff47a5e
Sahil Gupta <guptasahil2175@gmail.com>
2025-06-22 14:24:43 +0530

    Add Abundant number checker

---
+++ Java-Control-Flows/Level-3/Q9_AbundantNumber.java
@@ -0,0 +1,15 @@
+public class Q9_AbundantNumber {
+    public static void main(String[] args) {
+        int number = 12, sum = 0;
+        for (int i = 1; i < number; i++) {
+            if (number % i == 0) {
+                sum += i;
+            }
+        }
+        if (sum > number) {
+            System.out.println(number + " is an Abundant Number");
+        } else {
+            System.out.println(number + " is not an Abundant Number");
+        }
+    }
+}
