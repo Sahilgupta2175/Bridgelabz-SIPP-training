7e337aca46c069c5e09bc72b9ed1a222c9bf1795
Sahil Gupta <guptasahil2175@gmail.com>
2025-06-22 17:53:03 +0530

    [Sahil Gupta] Add Largest and second largest digit extraction

---
+++ Java-Arrays/Level-2/Q3_LargestTwoDigits.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+class Q3_LargestTwoDigits {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number: ");
+        int num = sc.nextInt();
+        int[] digits = new int[10];
+        int index = 0;
+
+        while (num != 0 && index < 10) {
+            digits[index++] = num % 10;
+            num /= 10;
+        }
+
+        int max1 = -1, max2 = -1;
+        for (int i = 0; i < index; i++) {
+            if (digits[i] > max1) {
+                max2 = max1;
+                max1 = digits[i];
+            } else if (digits[i] > max2 && digits[i] != max1) {
+                max2 = digits[i];
+            }
+        }
+
+        System.out.println("Largest: " + max1);
+        System.out.println("Second Largest: " + max2);
+        sc.close();
+    }
+}
