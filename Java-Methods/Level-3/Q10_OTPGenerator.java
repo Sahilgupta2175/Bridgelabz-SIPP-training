f5a3fdd1e689347d64a79166aa7198fb88a719d1
Sahil Gupta <guptasahil2175@gmail.com>
2025-06-23 11:59:09 +0530

    [Sahil Gupta] Add 10-digit OTP generator with uniqueness check

---
+++ Java-Methods/Level-3/Q10_OTPGenerator.java
@@ -0,0 +1,27 @@
+import java.util.HashSet;
+
+public class Q10_OTPGenerator {
+
+    public static int generateOTP() {
+        return (int) (Math.random() * 900000) + 100000;
+    }
+
+    public static boolean areOTPsUnique(int[] otps) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int otp : otps) {
+            if (!set.add(otp)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int[] otps = new int[10];
+        for (int i = 0; i < 10; i++) {
+            otps[i] = generateOTP();
+            System.out.println("OTP " + (i + 1) + ": " + otps[i]);
+        }
+        System.out.println("Are OTPs unique? " + areOTPsUnique(otps));
+    }
+}
