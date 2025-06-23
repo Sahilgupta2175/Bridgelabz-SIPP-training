1d2c79af145f1cba267d4f082672d9e2873c936b
Sahil Gupta <guptasahil2175@gmail.com>
2025-06-23 12:50:10 +0530

    [Sahil Gupta] Add Employee class with display method for employee details

---
+++ OOPS/Level-1/Q1_EmployeeDetails.java
@@ -0,0 +1,25 @@
+public class Q1_EmployeeDetails {
+
+    static class Employee {
+        String name;
+        int id;
+        double salary;
+
+        public Employee(String name, int id, double salary) {
+            this.name = name;
+            this.id = id;
+            this.salary = salary;
+        }
+
+        public void displayDetails() {
+            System.out.println("Employee Name: " + name);
+            System.out.println("Employee ID  : " + id);
+            System.out.println("Salary       : ₹" + salary);
+        }
+    }
+
+    public static void main(String[] args) {
+        Employee emp = new Employee("John Doe", 101, 50000);
+        emp.displayDetails();
+    }
+}
