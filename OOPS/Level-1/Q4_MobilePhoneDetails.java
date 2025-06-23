22b14404e21e08a4d538244cec81a94d6b19efe6
Sahil Gupta <guptasahil2175@gmail.com>
2025-06-23 12:50:13 +0530

    [Sahil Gupta] Add MobilePhone class to store and display mobile details

---
+++ OOPS/Level-1/Q4_MobilePhoneDetails.java
@@ -0,0 +1,25 @@
+public class Q4_MobilePhoneDetails {
+
+    static class MobilePhone {
+        String brand;
+        String model;
+        double price;
+
+        public MobilePhone(String brand, String model, double price) {
+            this.brand = brand;
+            this.model = model;
+            this.price = price;
+        }
+
+        public void displayDetails() {
+            System.out.println("Brand : " + brand);
+            System.out.println("Model : " + model);
+            System.out.println("Price : ₹" + price);
+        }
+    }
+
+    public static void main(String[] args) {
+        MobilePhone phone = new MobilePhone("Samsung", "Galaxy S21", 64999);
+        phone.displayDetails();
+    }
+}
