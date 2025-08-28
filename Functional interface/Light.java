260753b1050773042522be7a7592c3f4cfadddd5
Sahil Gupta <guptasahil2175@gmail.com>
2025-08-28 14:20:07 +0530

    [Sahil Gupta] Add Light class implementing SmartDeviceControl interface

---
+++ Functional interface/Light.java
@@ -0,0 +1,11 @@
+public class Light implements SmartDeviceControl {
+    @Override
+    public void turnOn() {
+        System.out.println("Light turned on");
+    }
+
+    @Override
+    public void turnOff() {
+        System.out.println("Light turned off");
+    }
+}
