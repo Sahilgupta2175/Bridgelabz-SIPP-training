a0376cd141473dedd29b445f0b2f1a80ad4d5484
Sahil Gupta <guptasahil2175@gmail.com>
2025-08-28 14:20:06 +0530

    [Sahil Gupta] Add Bike class implementing VehicleRental interface

---
+++ Functional interface/Bike.java
@@ -0,0 +1,11 @@
+public class Bike implements VehicleRental {
+    @Override
+    public void rent() {
+        System.out.println("Bike rented");
+    }
+
+    @Override
+    public void returnVehicle() {
+        System.out.println("Bike returned");
+    }
+}
