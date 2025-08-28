5845aaeb564d2236faa5caeafefe6e7e7ddcbb63
Sahil Gupta <guptasahil2175@gmail.com>
2025-08-28 14:12:53 +0530

    [Sahil Gupta] Add invoice object creation and management logic

---
+++ Lambda function/InvoiceObjectCreation.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.util.stream.*;
+
+class Invoice {
+    private String transactionId;
+
+    public Invoice(String transactionId) {
+        this.transactionId = transactionId;
+    }
+
+    public String toString() {
+        return "Invoice for: " + transactionId;
+    }
+}
+
+public class InvoiceObjectCreation {
+    public static void main(String[] args) {
+        List<String> transactionIds = Arrays.asList("TXN1001", "TXN1002", "TXN1003");
+        List<Invoice> invoices = transactionIds.stream()
+                .map(Invoice::new)
+                .collect(Collectors.toList());
+        System.out.println(invoices);
+    }
+}
