c43cb7e44f4aec3b171e13c9b03fa44e4065ac39
Sahil Gupta <guptasahil2175@gmail.com>
2025-08-28 14:04:53 +0530

    [Sahil Gupta] Add TemperatureConverter class for temperature unit conversions

---
+++ JUnit/src/TemperatureConverter.java
@@ -0,0 +1,9 @@
+public class TemperatureConverter {
+    public double celsiusToFahrenheit(double c) {
+        return c * 9.0 / 5.0 + 32.0;
+    }
+
+    public double fahrenheitToCelsius(double f) {
+        return (f - 32.0) * 5.0 / 9.0;
+    }
+}
