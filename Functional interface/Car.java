c238e80b9478bacba1a670ad2bd32f569458f825
Sahil Gupta <guptasahil2175@gmail.com>
2025-08-28 14:20:06 +0530

    [Sahil Gupta] Add Car class implementing VehicleRental interface

---
+++ Functional interface/Car.java
@@ -0,0 +1,11 @@
+public class Car implements VehicleRental {
+    @Override
+    public void rent() {
+        System.out.println("Car rented");
+    }
+
+    @Override
+    public void returnVehicle() {
+        System.out.println("Car returned");
+    }
+}
