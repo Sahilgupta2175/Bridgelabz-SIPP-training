56a01c480eb8ab48fa7d05ddd0f8c94f8fc2de8d
Sahil Gupta <guptasahil2175@gmail.com>
2025-08-28 14:20:06 +0530

    [Sahil Gupta] Add Bus class implementing VehicleRental interface

---
+++ Functional interface/Bus.java
@@ -0,0 +1,11 @@
+public class Bus implements VehicleRental {
+    @Override
+    public void rent() {
+        System.out.println("Bus rented");
+    }
+
+    @Override
+    public void returnVehicle() {
+        System.out.println("Bus returned");
+    }
+}
