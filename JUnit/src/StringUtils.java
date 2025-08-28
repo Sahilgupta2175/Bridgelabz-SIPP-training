c713837a94e8883e0cdf4d4c00f3e2e964e76bf5
Sahil Gupta <guptasahil2175@gmail.com>
2025-08-28 14:04:53 +0530

    [Sahil Gupta] Add StringUtils class with string manipulation utilities

---
+++ JUnit/src/StringUtils.java
@@ -0,0 +1,20 @@
+public class StringUtils {
+    public String reverse(String str) {
+        if (str == null)
+            return null;
+        return new StringBuilder(str).reverse().toString();
+    }
+
+    public boolean isPalindrome(String str) {
+        if (str == null)
+            return false;
+        String cleaned = str.replaceAll("\\s+", "").toLowerCase();
+        return cleaned.equals(reverse(cleaned));
+    }
+
+    public String toUpperCase(String str) {
+        if (str == null)
+            return null;
+        return str.toUpperCase();
+    }
+}
